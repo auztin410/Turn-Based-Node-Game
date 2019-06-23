e4fa09021e4f125a9948594a22ab95643e3b02e8
sugarcube <auztin410@gmail.com>
2019-06-23 08:23:46 -0400

    Setting up objects for character and creatures.

---
+++ game.js
@@ -0,0 +1,41 @@
+var inquirer = require("inquirer");
+
+var Character = {
+    Name = "",
+    Level = 0,
+    Class = null,
+    Items = [],
+    Experience = 0,
+    Weapon = null,
+    Shield = null,
+    Armor = null,
+        Strength = 0,
+        Dexterity = 0,
+        Constitution = 0,
+        Intelligence = 0,
+        Charisma = 0,
+        Wisdom = 0,
+        Speed = 0,
+        Armor = 0,
+        Resistance = 0,
+        Dodge = 0,
+        Parry = 0,
+        Block = 0
+}
+
+var Creature = {
+    Name = "",
+    Items = [],
+        Strength = 0,
+        Dexterity = 0,
+        Constitution = 0,
+        Intelligence = 0,
+        Charisma = 0,
+        Wisdom = 0,
+        Speed = 0,
+        Armor = 0,
+        Resistance = 0,
+        Dodge = 0,
+        Parry = 0,
+        Block = 0
+}
